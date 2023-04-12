31bf020c90c72b4d433ce68110d21ed38744c966
pennitt <120532339+pennitt@users.noreply.github.com>
2023-04-12 11:38:04 +0900

    [Bronze I] Title: 최소공배수, Time: 4988 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -0,0 +1,18 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+for i in range(n):
+    a, b = map(int, sys.stdin.readline().split())
+    a_lst = [a // i for i in range(1, a+1) if a % i == 0]
+    b_lst = [b // i for i in range(1, b+1) if b % i == 0]
+
+    s1 = set(a_lst)
+    s2 = set(b_lst)
+    s = s1 & s2
+
+    max_n = max(s)
+    
+    result = max_n * (a // max_n) * (b // max_n)
+
+    print(int(result))
