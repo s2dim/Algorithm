8945cd90fb04e03167714225bbbe2fc89b0a323e
pennitt <120532339+pennitt@users.noreply.github.com>
2024-08-05 22:38:25 +0900

    [Bronze I] Title: 딱지놀이, Time: 176 ms, Memory: 17712 KB, Score: 15 point -BaekjoonHub

---
+++ 백준/Bronze/14696. 딱지놀이/딱지놀이.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// 별 4 동그라미 3 네모 2 세모 1
+
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+
+		for (int i = 0; i < N; i++) {
+			int[] a_card = new int[5];
+			int[] b_card = new int[5];
+
+			int a = sc.nextInt();
+			for (int j = 0; j < a; j++) {
+				int score = sc.nextInt();
+				a_card[score]++;
+			}
+
+			int b = sc.nextInt();
+			for (int j = 0; j < b; j++) {
+				int score = sc.nextInt();
+				b_card[score]++;
+
+			}
+
+			int a_sum = a_card[1] * 1 + a_card[2] * 1000 + a_card[3] * 1000000 + a_card[4] * 1000000000;
+			int b_sum = b_card[1] * 1 + b_card[2] * 1000 + b_card[3] * 1000000 + b_card[4] * 1000000000;
+
+			if (a_sum > b_sum)
+				System.out.println("A");
+			else if (b_sum > a_sum)
+				System.out.println("B");
+			else
+				System.out.println("D");
+		}
+
+	}
+}
