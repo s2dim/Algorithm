6a3721e3ef148bd46222e837b3c7f3ce8fb272dc
pennitt <120532339+pennitt@users.noreply.github.com>
2024-08-20 17:18:04 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 92 ms, Memory: 12840 KB -BaekjoonHub

--- 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -1,18 +1,19 @@
-import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        System.out.println(factorial(num));
-    }
+	public static void main(String[] args) {
 
-    static BigInteger factorial(int num) {
-        if (num > 1) {
-            return factorial(num - 1).multiply(BigInteger.valueOf(num));
-        } else {
-            return BigInteger.ONE;
-        }
-    }
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		System.out.println(factorial(num));
+		
+	}
+
+	static long factorial(int num) {
+		if (num > 1) {
+			return factorial(num - 1) * num;
+		} else {
+			return 1;
+		}
+	}
 }
