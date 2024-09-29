934431676d25bbb494f5d98783323e1465afb592
pennitt <120532339+pennitt@users.noreply.github.com>
2024-09-30 01:30:46 +0900

    [D4] Title: 치즈 도둑, Time: 477 ms, Memory: 103,612 KB -BaekjoonHub

--- SWEA/D4/7733. 치즈 도둑/치즈 도둑.java
+++ SWEA/D4/7733. 치즈 도둑/치즈 도둑.java
@@ -1,16 +1,15 @@
-
 import java.util.*;
 
 public class Solution {
 
 	static int n;
 	static int[][] arr;
+	static int day;
+	static int[] dx = { 1, -1, 0, 0 };
+	static int[] dy = { 0, 0, 1, -1 };
 	static boolean[][] visited;
-
-	// 상 하 좌 우
-	static int[] dx = { -1, 1, 0, 0 };
-	static int[] dy = { 0, 0, -1, 1 };
-
+	static int cnt;
+	
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
@@ -19,10 +18,9 @@ public class Solution {
 
 		for (int t = 1; t <= T; t++) {
 			n = sc.nextInt();
-
 			arr = new int[n][n];
 
-			// 배열 생성
+			// 배열에 입력 받음
 			for (int i = 0; i < n; i++) {
 				for (int j = 0; j < n; j++) {
 					arr[i][j] = sc.nextInt();
@@ -30,59 +28,52 @@ public class Solution {
 			}
 
 			int max = 1;
-			int day = 1;
-
-			// 면적이 다 사라질 때까지 반복 수행
-			int all = n * n;
-			while (all != 0) {
+			day = 1;
+			while (day <= 100) {
+				
+				cnt = 0;
 				visited = new boolean[n][n];
+				
 				for (int i = 0; i < n; i++) {
 					for (int j = 0; j < n; j++) {
-						if (arr[i][j] == day) {
-							arr[i][j] = 0;
-							all--;
-						}
-					}
-				}
-				day++;
-
-				int cnt = 0;
-				for (int i = 0; i < n; i++) {
-					for (int j = 0; j < n; j++) {
-						if (arr[i][j] > 0 && !visited[i][j]) {
+						if (!visited[i][j] && arr[i][j] > day) {
+							BFS(i, j);
 							cnt++;
-							bfs(i, j);
 						}
 					}
 				}
-
+				
+				day++;
 				max = Math.max(max, cnt);
 			}
+			
 			System.out.println("#" + t + " " + max);
 		}
-		sc.close();
+
 	}
 
-	static void bfs(int x, int y) {
+	static void BFS(int x, int y) {
 		Queue<int[]> queue = new LinkedList<>();
+
 		queue.add(new int[] { x, y });
-		visited[x][y] = true;
+		visited[x][y] = true; 
 
 		while (!queue.isEmpty()) {
 			int[] curr = queue.poll();
 			int currX = curr[0];
 			int currY = curr[1];
-
+//			cnt++;
+			
 			for (int d = 0; d < 4; d++) {
 				int nx = currX + dx[d];
 				int ny = currY + dy[d];
 
-				if (nx >= 0 && nx < n && ny >= 0 && ny < n && arr[nx][ny] > 0 && !visited[nx][ny]) {
+				if (nx >= 0 && nx < n && ny >= 0 && ny < n && !visited[nx][ny] && arr[nx][ny] > day) {
 					queue.add(new int[] { nx, ny });
 					visited[nx][ny] = true;
 				}
 			}
 		}
-
 	}
+
 }
