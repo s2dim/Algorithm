7499afc79d5973190d57677de647685ec5a2b4ec
pennitt <120532339+pennitt@users.noreply.github.com>
2024-09-30 05:07:05 +0900

    [Gold IV] Title: 동전 1, Time: 104 ms, Memory: 13020 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -0,0 +1,33 @@
+
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		
+		int[] coin = new int[n];
+		int[] dp = new int[k+1];
+		
+		for (int i = 0; i < n; i++) {
+			coin[i] = sc.nextInt();
+			
+		}
+		
+		dp[0] = 1;
+		
+		if (n >= 1) {
+			for (int i = 0; i < n; i++) {
+				for (int j = coin[i]; j <= k; j++) {
+					dp[j] = dp[j] + dp[j - coin[i]];
+				}
+			}
+		}
+		System.out.println(dp[k]);
+	}
+
+}
