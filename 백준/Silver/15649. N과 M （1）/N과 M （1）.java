3775f6b094e30c7842ac86b711155c811d9435ab
pennitt <120532339+pennitt@users.noreply.github.com>
2024-12-10 00:02:41 +0900

    [Silver III] Title: N과 M (1), Time: 1816 ms, Memory: 79276 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,52 +1,45 @@
 
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
-
-	static int N;
-	static int M;
-
-	static boolean [] visited;
-	static int [] arr;
 	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		N = Integer.parseInt(st.nextToken());
+	static int n, m;
+	static boolean visited[];
+	static int[] arr;
+	
+	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		
-
-		M = Integer.parseInt(st.nextToken());
+		n = sc.nextInt();
+		m = sc.nextInt();
 		
-		visited = new boolean[N+1];
-		arr = new int[M];
-
-		dfs(0);
-
+		visited = new boolean[n];
+		arr = new int[m];
+		
+		cal(0);
+		
 	}
-
-	static void dfs(int cnt) {
-		if(cnt==M) {
-			for (int i : arr) {
-				System.out.print(i+" ");
+	
+	static void cal(int depth) {
+		
+		if (depth == m) {
+			for (int i = 0; i < m; i++) {
+				System.out.print(arr[i] + " ");
 			}
 			System.out.println();
 			return;
 		}
 		
-		for(int i=1; i<N+1; i++) {
-			if(!visited[i]) {
+		for (int i = 0; i < n; i++) {
+			if (!visited[i]) {
 				visited[i] = true;
-				arr[cnt] = i;
-				dfs(cnt+1);
+				arr[depth] = i + 1;
+				cal(depth + 1);
 				visited[i] = false;
 			}
 		}
 	}
+
 }
