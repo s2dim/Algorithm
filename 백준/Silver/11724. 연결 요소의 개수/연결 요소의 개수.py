feecafab8b2b5837f0b21a3ed7d8e88de7499e22
s2dim <120532339+s2dim@users.noreply.github.com>
2025-07-27 16:23:58 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 576 ms, Memory: 39308 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,26 +1,26 @@
+
 import sys
 sys.setrecursionlimit(10000)
 
 n, m = map(int, sys.stdin.readline().split())
 
-A = [[] for i in range(n+1)]
+arr = [[0] * (n + 1) for _ in range(n + 1)]
+visited = [False] * (n + 1)
 
-visited = [False] * (n+1)
+for i in range(m):
+    a, b = map(int, sys.stdin.readline().split())
+    arr[a][b] = 1
+    arr[b][a] = 1
 
 def DFS(v):
     visited[v] = True
-    for i in A[v]:
-        if not visited[i]:
+    for i in range(1, n + 1):
+        if not visited[i] and arr[v][i] == 1:
             DFS(i)
-
-for i in range(m):
-    s, e = map(int, sys.stdin.readline().split())
-    A[s].append(e)
-    A[e].append(s)
 
 count = 0
 
-for i in range(1, n+1):
+for i in range(1, n + 1):
     if not visited[i]:
         count += 1
         DFS(i)
