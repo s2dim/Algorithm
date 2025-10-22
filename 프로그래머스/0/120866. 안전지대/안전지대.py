2c3fa26079178bb85d60958eef4ea5b4f3a5327a
s2dim <120532339+s2dim@users.noreply.github.com>
2025-10-22 23:40:42 +0900

    [level 0] Title: 안전지대, Time: 0.07 ms, Memory: 9.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/120866. 안전지대/안전지대.py
@@ -0,0 +1,24 @@
+def solution(board):
+    dx = [0, 0, -1, -1, -1, 1, 1, 1]
+    dy = [-1, 1, -1, 1, 0, -1, 1, 0]
+    
+    n = len(board)
+    visited = [([False] * n) for _ in range(n)]
+
+    answer = n * n
+    
+    for i in range(n):
+        for j in range(n):
+            if (board[i][j] == 1 and not visited[i][j]):
+                answer -= 1
+                visited[i][j] = True
+                
+                for d in range(8):
+                    nx = i + dx[d]
+                    ny = j + dy[d]
+                    if (nx >= 0 and nx < n and ny >= 0 and ny < n and 
+                        board[nx][ny] == 0 and not visited[nx][ny]):
+                        answer -= 1
+                        visited[nx][ny] = True
+
+    return answer
