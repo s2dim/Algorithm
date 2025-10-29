8b7aa1b621c6405a61248e243f97c58301fc64b3
s2dim <120532339+s2dim@users.noreply.github.com>
2025-10-29 11:52:11 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1652 ms, Memory: 101360 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,43 +1,31 @@
 import sys
-import heapq
+from collections import deque
+
 n, m, k, x = map(int, sys.stdin.readline().split())
 
 inf = int(1e9)
 graph = [[] for _ in range(n+1)]
-distance = [inf] * (n+1)
+distance = [-1] * (n+1)
 
-# 다익스트라
 for i in range(m):
     a, b = map(int, sys.stdin.readline().split())
-    graph[a].append((b, 1))
+    graph[a].append((b))
 
+queue = deque([x])
+distance[x] = 0
 
-def dijkstra(start):
-    queue = []
-    heapq.heappush(queue, (0, start))
-    distance[start] = 0
+while queue:
+    q = queue.popleft()
 
-    while queue:
-        dist, now = heapq.heappop(queue)
-        if distance[now] < dist:
-            continue
+    for i in graph[q]:
+        if distance[i] == -1:
+            distance[i] = distance[q] + 1
+            queue.append(i)
 
-        for i in graph[now]:
-            to_v = i[0]
-            cost = dist + i[1]
-
-            if cost < distance[to_v]:
-                distance[to_v] = cost
-                heapq.heappush(queue, (cost, to_v))
-
-
-    return distance
-
-arr = dijkstra(x)
 answer = []
 
-for i in range(len(arr)):
-    if arr[i] == k:
+for i in range(1, n+1):
+    if distance[i] == k:
         answer.append(i)
 
 if len(answer) == 0:
@@ -46,3 +34,6 @@ else:
     answer.sort()
     for i in answer:
         print(i)
+
+
+
