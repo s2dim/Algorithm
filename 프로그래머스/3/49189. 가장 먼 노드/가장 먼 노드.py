b3bfcdb1b7a7c8ff246890ef81ecda1eeeb4e73a
s2dim <120532339+s2dim@users.noreply.github.com>
2025-10-30 23:57:03 +0900

    [level 3] Title: 가장 먼 노드, Time: 79.88 ms, Memory: 26.2 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,38 +1,35 @@
 import heapq
 from collections import Counter
+
 def solution(n, vertex):
     
     inf = int(1e9)
     graph = [[] for _ in range(n+1)]
     distance = [inf] * (n+1)
     
-    for i in vertex:
-        a, b = i
+    for a, b in vertex:
         graph[a].append((b, 1))
         graph[b].append((a, 1))
-        
-    queue = []
-    heapq.heappush(queue, (0, 1))
+    
+    heap = []
+    heapq.heappush(heap, (0, 1))
     distance[1] = 0
     
-    while queue:
-        dist, now = heapq.heappop(queue)
+    while heap:
+        dist, now = heapq.heappop(heap)
+        
         if distance[now] < dist:
             continue
-
+        
         for i in graph[now]:
             to_v = i[0]
             cost = dist + i[1]
             
             if cost < distance[to_v]:
                 distance[to_v] = cost
-                heapq.heappush(queue, (cost, to_v))
-
-    lst = Counter(distance[1:])
-
-    return lst[max(lst)]
-
-
-'''
-
-'''
+                heapq.heappush(heap, (cost, to_v))
+                
+    max_ = max(distance[1:])
+    cnt = Counter(distance[1:])
+    
+    return cnt.get(max_)
