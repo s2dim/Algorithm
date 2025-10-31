6958428d57217abc1f19dd5a9d37ebfb28f6a54c
s2dim <120532339+s2dim@users.noreply.github.com>
2025-10-31 22:30:50 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,5 +1,7 @@
 -- 코드를 입력하세요
 SELECT i.name, i.datetime
-from animal_ins as i left join animal_outs as o on i.animal_id = o.animal_id
+from animal_ins i
+left join animal_outs o on i.animal_id = o.animal_id
 where o.datetime is null
-order by i.datetime limit 3
+order by i.datetime asc
+limit 3
