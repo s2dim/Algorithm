26b118693e7e95ef6ebfe62a07f490b05e4064bf
s2dim <120532339+s2dim@users.noreply.github.com>
2025-11-01 00:14:58 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,11 @@
+-- 코드를 입력하세요
+SELECT board_id, writer_id, title, price, (
+case
+when status = 'DONE' then '거래완료'
+when status = 'RESERVED' then '예약중'
+when status = 'SALE' then '판매중'
+else '거래완료'
+end) status
+from used_goods_board
+where created_date like '2022-10-05'
+order by board_id desc
