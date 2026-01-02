6e37f14926bee373796ccbcf2edf6325bd8070a1
s2dim <120532339+s2dim@users.noreply.github.com>
2026-01-02 18:38:20 +0900

    [level 2] Title: 타겟 넘버, Time: 357.81 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,38 +1,14 @@
-def Plus(a, b):
-    return a + b
-
-def Minus(a, b):
-    return a - b
-
 def solution(numbers, target):
-    
-    if sum(numbers) == target:
-        return 1
-    
-    cnt = 0
-    stack = [numbers[0], -numbers[0]]
-    
-    for i in range(1, len(numbers)):
+    result = 0
+    def dfs(i, now):
+        nonlocal result
+        if i == len(numbers):
+            if now == target:
+                result += 1
+            return 
         
-        temp = []
+        dfs(i+1, now + numbers[i])
+        dfs(i+1, now - numbers[i])
         
-        while stack:
-            a = stack.pop()
-            
-            p = Plus(a, numbers[i])
-            m = Minus(a, numbers[i])
-            
-            # 스택에 담기
-            temp.append(p)
-            temp.append(m)
-
-        stack = temp
-    answer = stack.count(target)
-    return answer
-
-'''
-
-값이 됐는데 남은 숫자가 더 있음 -> 탈출
-
-
-'''
+    dfs(0, 0)
+    return result
