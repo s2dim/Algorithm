9b6541ce16bd005ddbaba317d6abc7903a5a2300
s2dim <120532339+s2dim@users.noreply.github.com>
2026-01-04 17:49:28 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -1,12 +1,8 @@
 -- 코드를 입력하세요
-SELECT r.food_type, rest_id, rest_name, favorites
-from rest_info r
-join (select food_type, max(favorites) as max
-      from rest_info
-      group by food_type) fv
-on    r.food_type = fv.food_type and r.favorites = fv.max
+SELECT food_type, rest_id, rest_name, favorites
+from rest_info
+where favorites in (SELECT max(favorites) as favorite
+                    from rest_info
+                    group by food_type)
 group by food_type
 order by food_type desc
-
-
-
