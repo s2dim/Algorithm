437e3d9372138ea6d82188fb72ba92cee3f74a9e
s2dim <120532339+s2dim@users.noreply.github.com>
2026-01-04 23:54:09 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
+++ 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -1,23 +1,10 @@
 -- 코드를 작성해주세요
-
-# select id, ntile(4) over (order by size_of_colony) as class
-# from ecoli_data
-
-# case class
-# when 1 then 'LOW'
-# when 2 then 'MEDIUM'
-# when 3 then 'HIGH'
-# when 4 then 'CRITICAL'
-# end
-
-select d.id, case c.class
-                when 1 then 'LOW'
-                when 2 then 'MEDIUM'
-                when 3 then 'HIGH'
-                when 4 then 'CRITICAL'
-                end as colony_name
-from ecoli_data d
-join (select id, ntile(4) over (order by size_of_colony) as class
-      from ecoli_data) c
-on d.id = c.id
+select a.id, (case
+              when b.per <= 0.25 then 'CRITICAL'
+              when b.per <= 0.50 then 'HIGH'
+              when b.per <= 0.75 then 'MEDIUM'
+              else 'LOW'
+          end) as colony_name
+from ecoli_data a join (select id, percent_rank() over (order by size_of_colony desc) as per
+                        from ECOLI_DATA ) b on a.id = b.id
 order by id
