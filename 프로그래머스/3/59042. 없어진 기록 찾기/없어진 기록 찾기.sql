f308cc627766f72bd89063212e9bec1388f750e1
s2dim <120532339+s2dim@users.noreply.github.com>
2026-01-04 18:30:18 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,6 +1,4 @@
 -- 코드를 입력하세요
 SELECT o.animal_id, o.name
-from ANIMAL_INS i
-right join ANIMAL_OUTS o
-on i.animal_id = o.animal_id
-where o.datetime is not null and i.datetime is null
+from animal_outs o left join animal_ins i on o.animal_id = i.animal_id
+where i.datetime is null
