7f829f62f08438629d58be1a678d45140ad2006a
s2dim <120532339+s2dim@users.noreply.github.com>
2026-01-04 17:42:55 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -1,5 +1,7 @@
 -- 코드를 입력하세요
 SELECT i.animal_id, i.name
-from animal_ins as i left join animal_outs as o on i.animal_id = o.animal_id
-where o.datetime is not null
-order by (o.datetime - i.datetime) desc limit 2
+from animal_ins i right join (SELECT i.animal_id, o.datetime - i.datetime as period
+                            from animal_ins i join animal_outs o on i.animal_id = o.animal_id
+                            order by period desc
+                            limit 2) p on i.animal_id = p.animal_id
+order by p.period desc
