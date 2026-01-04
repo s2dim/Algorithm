190298d456a8b4424327f2e247dc90163d1f95db
s2dim <120532339+s2dim@users.noreply.github.com>
2026-01-04 18:52:15 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,8 +1,6 @@
 -- 코드를 입력하세요
-select concat('/home/grep/src/', f.board_id,'/', file_id, file_name, file_ext) as file_path
-from used_goods_file f
-join (SELECT board_id
-      from USED_GOODS_BOARD
-      order by views desc limit 1) v
-on f.board_id = v.board_id
+SELECT concat('/home/grep/src/', b.board_id, '/', file_id, file_name, file_ext) as file_path
+from used_goods_board b join used_goods_file f on b.board_id = f.board_id
+where b.views = (SELECT max(views)
+                from USED_GOODS_BOARD)
 order by file_id desc
