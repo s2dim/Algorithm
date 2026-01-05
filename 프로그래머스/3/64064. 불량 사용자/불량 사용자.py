73aea5c34a18f056f27eff703af4649635a64ff4
s2dim <120532339+s2dim@users.noreply.github.com>
2026-01-05 23:25:51 +0900

    [level 3] Title: 불량 사용자, Time: 556.37 ms, Memory: 20.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
@@ -0,0 +1,52 @@
+from collections import deque, Counter
+from itertools import permutations
+
+def solution(user_id, banned_id):
+    answer = 0
+    
+    def diff(a, b):
+        if len(a) != len(b):
+            return False
+        else:
+            a = [i for i in a]
+            b = [i for i in b]
+            
+            for i in range(len(a)):
+                if b[i] == '*' or a[i] == b[i]:
+                    continue
+                else:
+                    return False
+            return True
+    
+    lst = []
+    for i in list(permutations(user_id, len(banned_id))):
+        cnt = 0
+        for j in range(len(banned_id)):
+            if not diff(i[j], banned_id[j]):
+                break
+            else:
+                cnt += 1
+        if cnt == len(banned_id):
+            temp = [k for k in i]
+            lst.append(sorted(temp))
+    
+    lst.sort()
+    cnt = 1
+    idx = 1
+    prev = lst[0]
+    while idx < len(lst):
+        
+        if prev != lst[idx]:
+            prev = lst[idx]
+            cnt += 1
+            idx += 1
+        else:
+            idx += 1
+    return cnt
+
+'''
+- 아이디 당 최소 하나 이상 *
+- 제재 아이디 목록 몇 가지 경우의 수?
+
+
+'''
