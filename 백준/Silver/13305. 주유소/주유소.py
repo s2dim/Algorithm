c572a3a001cef4c374bcef72fd717346074b9e47
s2dim <120532339+s2dim@users.noreply.github.com>
2026-03-29 11:45:38 +0900

    [Silver III] Title: 주유소, Time: 108 ms, Memory: 48604 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -1,20 +1,13 @@
 import sys
 
-n = int(input()) # bank의 개수
-oil = list(map(int, sys.stdin.readline().split())) # 필요 기름
-bank = list(map(int, sys.stdin.readline().split())) # 주유소 가격
+n = int(input()) # oil 개수
+distance = list(map(int, sys.stdin.readline().split())) # 필요 기름
+oil = list(map(int, sys.stdin.readline().split())) # 주유소 가격
 
-idx = 0 # bank에 대한 idx
 cost = 0
+cheap = oil[0]
 
-while idx < n-1:
-
-    for i in range(idx + 1, n):
-        if bank[idx] >= bank[i]:
-            cost += sum(oil[idx:i]) * bank[idx]
-            idx = i
-            break
-
-if cost == 0:
-    cost = sum(oil) * bank[idx]
+for i in range(n-1):
+    cheap = min(cheap, oil[i])
+    cost += cheap * distance[i]
 print(cost)
