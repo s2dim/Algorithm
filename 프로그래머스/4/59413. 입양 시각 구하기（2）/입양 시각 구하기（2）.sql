6a9032736d545e6a675764e33cd49bb68cacf103
s2dim <120532339+s2dim@users.noreply.github.com>
2026-03-31 02:39:30 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,11 +1,13 @@
-with recursive hours as (
+-- 코드를 입력하세요
+with recursive hours as(
     select 0 as hour
     union all
     select hour + 1
     from hours
-    where hour < 23)
-
-select h.hour as hour, count(o.datetime) as count
-from hours h left join animal_outs o on h.hour = hour(o.datetime)
+    where hour < 23
+)
+select h.hour, count(o.hour) as count
+from hours h left join (select hour(datetime) as hour
+                 from animal_outs) o on h.hour = o.hour
 group by h.hour
-order by hour
+order by h.hour
