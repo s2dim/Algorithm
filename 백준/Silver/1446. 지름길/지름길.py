9d2b6eec18609b9546d296e779bb80b500b363f2
s2dim <120532339+s2dim@users.noreply.github.com>
2026-03-30 11:54:17 +0900

    [Silver I] Title: 지름길, Time: 40 ms, Memory: 35508 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.py
+++ 백준/Silver/1446. 지름길/지름길.py
@@ -1,30 +1,40 @@
 import sys
-sys.setrecursionlimit(10**6)
+import heapq
 
 n, d = map(int, sys.stdin.readline().split())
 
 lst = []
 graph = [[] for i in range(d+1)]
+inf = int(1e9)
+distance = [inf] * (d+1)
+
+for i in range(d):
+    graph[i].append((i+1, 1))
 
 for i in range(n):
     a, b, c = map(int, sys.stdin.readline().split())
     if a <= d and b <= d:
         graph[a].append((b, c))
 
+heap = []
+heapq.heappush(heap, (0, 0))
+distance[0] = 0
 
-def shortcut(idx, cost):
-    if idx == d:
-        return cost
-    if idx > d:
-        return int(1e9)
-    result = shortcut(idx+1, cost+1)
-    
-    if graph[idx]:
-        for i in graph[idx]:
-            result = min(result, shortcut(i[0], cost+i[1]))
-    return result
+while heap:
+    dist, now = heapq.heappop(heap)
+    if distance[now] < dist:
+        continue
 
-print(shortcut(0, 0))
+    for i in graph[now]:
+        to_v = i[0]
+        cost = dist + i[1]
+
+        if cost < distance[to_v]:
+            distance[to_v] = cost
+            heapq.heappush(heap, (cost, to_v))
+
+print(distance[d])
+
 
 '''
 지름길
