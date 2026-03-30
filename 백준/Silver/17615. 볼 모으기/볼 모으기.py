8bff7f303bdfeff4bac8949f676ca0f022e0e722
s2dim <120532339+s2dim@users.noreply.github.com>
2026-03-30 19:45:44 +0900

    [Silver I] Title: 볼 모으기, Time: 136 ms, Memory: 33392 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/17615. 볼 모으기/볼 모으기.py
+++ 백준/Silver/17615. 볼 모으기/볼 모으기.py
@@ -1,53 +1,48 @@
-import sys
+n = int(input())
+color = input()
 
-n = int(sys.stdin.readline())
-color = list(map(str, sys.stdin.readline().strip()))
+# 전체 개수
+total_r = color.count('R')
+total_b = color.count('B')
 
-min_ = int(1e9)
-cnt = 0
-temp = 0
+# 왼쪽에서 연속된 R
+left_r = 0
+for c in color:
+    if c == 'R':
+        left_r += 1
+    else:
+        break
 
-#오른쪽 보내기
-for i in range(n):
-    if color[i] == 'R':
-        temp += 1
-    if color[i] == 'B' and temp:
-        cnt += temp
-        temp = 0
+# 오른쪽에서 연속된 R
+right_r = 0
+for c in reversed(color):
+    if c == 'R':
+        right_r += 1
+    else:
+        break
 
-min_ = min(min_, cnt)
+# 왼쪽에서 연속된 B
+left_b = 0
+for c in color:
+    if c == 'B':
+        left_b += 1
+    else:
+        break
 
-cnt = 0
-temp = 0
-color.reverse()
-for i in range(n):
-    if color[i] == 'R':
-        temp += 1
-    if color[i] == 'B' and temp:
-        cnt += temp
-        temp = 0
+# 오른쪽에서 연속된 B
+right_b = 0
+for c in reversed(color):
+    if c == 'B':
+        right_b += 1
+    else:
+        break
 
-min_ = min(min_, cnt)
+# 4가지 경우 중 최소
+answer = min(
+    total_r - left_r,
+    total_r - right_r,
+    total_b - left_b,
+    total_b - right_b
+)
 
-cnt = 0
-temp = 0
-for i in range(n):
-    if color[i] == 'B':
-        temp += 1
-    if color[i] == 'R' and temp:
-        cnt += temp
-        temp = 0
-min_ = min(min_, cnt)
-
-color.reverse()
-cnt = 0
-temp = 0
-for i in range(n):
-    if color[i] == 'B':
-        temp += 1
-    if color[i] == 'R' and temp:
-        cnt += temp
-        temp = 0
-min_ = min(min_, cnt)
-
-print(min_)
+print(answer)
