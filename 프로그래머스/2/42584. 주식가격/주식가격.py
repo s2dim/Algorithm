bda5effdddebe815bff0577652311cfc0f22c3c2
s2dim <120532339+s2dim@users.noreply.github.com>
2026-06-29 18:48:09 +0900

    [level 2] Title: 주식가격, Time: 97.27 ms, Memory: 20.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,13 +1,18 @@
+from collections import deque
+
 def solution(prices):
-    
-    answer = [0] * (len(prices))
-    start = 0
-    for i in range(len(prices)-1):
-        for j in range(i+1, len(prices)):
-            if j != len(prices):
-                answer[i] += 1
-            if prices[i] <= prices[j]:
-                continue
+    answer = [0] * len(prices)
+    check = deque([i for i in range(len(prices))])
+
+    while check:
+        q = check.popleft()
+        cnt = 0
+        for i in range(q+1, len(prices)):
+            if prices[q] <= prices[i]:
+                cnt += 1
             else:
+                cnt += 1
                 break
+        answer[q] += cnt
+        
     return answer
