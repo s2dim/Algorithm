2cc6dc1a70743de2c1d56eaa9bfadae936caf186
s2dim <120532339+s2dim@users.noreply.github.com>
2026-06-30 02:16:04 +0900

    [level 2] Title: 큰 수 만들기, Time: 41.29 ms, Memory: 14.5 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -2,22 +2,16 @@ def solution(number, k):
     answer = []
     
     for now in number:
-        while answer and k > 0 and answer[-1] < now:
-            answer.pop()
-            k -= 1
-        
+        while answer and k > 0:
+            if now > answer[-1]:
+                answer.pop()
+                k -= 1
+            else:
+                break
+                
         answer.append(now)
-    
+        
     if k > 0:
         answer = answer[:-k]
-    
+            
     return ''.join(answer)
-'''
-가장 큰 수 찾아냄
-k개 지워서 닿을 수 있는지 판단
-이후 k:에서 작은 수부터 지우기
-중복이 있을 경우엔 어떡하지
-
-1 9 2 4
-0 1 2 3
-'''
