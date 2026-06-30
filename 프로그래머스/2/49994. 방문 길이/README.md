6ae5d948a7450c792dd8330c7ee838c54e9bebf7
s2dim <120532339+s2dim@users.noreply.github.com>
2026-06-30 18:15:34 +0900

    [level 2] Title: 방문 길이, Time: 0.17 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/README.md
+++ 프로그래머스/2/49994. 방문 길이/README.md
@@ -1,10 +1,10 @@
 # [level 2] 방문 길이 - 49994 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49994) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49994#) 
 
 ### 성능 요약
 
-메모리: 11.6 MB, 시간: 0.34 ms
+메모리: 11.6 MB, 시간: 0.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 30일 17:46:17
+2026년 06월 30일 18:15:29
 
 ### 문제 설명
 
--- 프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -1,54 +1,15 @@
 def solution(dirs):
-    # 배열 재정의 0~11 * 0~11 (시작점 5, 5)
-    arr = [[[] for i in range(11)] for j in range(11)]
-    answer = 0
+    move = {'U':(0, 1), 'D':(0, -1), 'L':(-1, 0), 'R':(1,0)}
+    visited = set()
+    x, y = 0, 0
     
-    def check(nx, ny):
-        if nx >= 0 and nx < 11 and ny >= 0 and ny < 11:
-            return True
-        else:
-            return False
-        
-    def direction(d, x, y):
-        if d == 'L':
-            nx = x
-            ny = y - 1
-        elif d == 'R':
-            nx = x
-            ny = y + 1
-        elif d == 'U':
-            nx = x - 1
-            ny = y
-        elif d == 'D':
-            nx = x + 1
-            ny = y
-            
-        if check(nx, ny):
-            return [nx, ny]
-        else:
-            return [x, y]
-        
-    nowx, nowy = (5, 5)
     for d in dirs:
-        old = (nowx, nowy)
-        nowx, nowy = direction(d, nowx, nowy)
+        dx, dy = move[d]
+        nx = x + dx
+        ny = y + dy
         
-        if old != (nowx, nowy):
-            state = False
-            for i in arr[nowx][nowy]:
-                if i == old:
-                    state = True
-                    break
-            for i in arr[old[0]][old[1]]:
-                if i == (nowx, nowy):
-                    state = True
-                    break
-                    
-            if not state:
-                arr[nowx][nowy].append(old)
-                arr[old[0]][old[1]].append((nowx, nowy))
-                answer += 1
-        
-
-
-    return answer
+        if -5 <= nx <= 5 and -5 <= ny <= 5:
+            visited.add(((nx, ny), (x, y)))
+            visited.add(((x, y), (nx, ny)))
+            x, y = nx, ny
+    return len(visited) // 2
