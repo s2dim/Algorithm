d61b9213bced933610b20335fb228c44bd11858d
s2dim <120532339+s2dim@users.noreply.github.com>
2026-07-01 19:29:03 +0900

    Time: 688 ms (84.12%), Space: 0B (100%) - LeetHub

--- LeetCode/Medium/1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ LeetCode/Medium/1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,6 +1,10 @@
 # Write your MySQL query statement below
 
-select s.product_id, t.year as first_year, s.quantity as quantity, s.price as price
-from sales s join (select product_id, min(year) as year
-                    from sales
-                    group by product_id) t on s.product_id = t.product_id and s.year = t.year
+with first as(
+    select product_id, year, rank() over (partition by product_id order by year) as row_year, quantity, price
+    from sales
+)
+
+select product_id, year as first_year, quantity, price
+from first
+where row_year = 1
