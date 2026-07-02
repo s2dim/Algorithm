60d60648068354a6367f9ba5d9cab883a149f72b
s2dim <120532339+s2dim@users.noreply.github.com>
2026-07-02 20:36:22 +0900

    [level 2] Title: 오픈채팅방, Time: 94.76 ms, Memory: 58.5 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -1,35 +1,23 @@
 def solution(record):
-    
-    def message(ent, userid):
-        if ent == 'Enter':
-            text = f"{dic.get(userid)}님이 들어왔습니다."
-        elif ent == 'Leave':
-            text = f"{dic.get(userid)}님이 나갔습니다."
-        return text
-    
-    dic = {}
-    state = []
+    lst = []
+    members = {}
+    answer = []
     
     for i in record:
-        if i.startswith('Enter'):
-            ent, userid, nick = i.split()
-            state.append([ent, userid])
-            dic[userid] = nick
-        elif i.startswith('Leave'):
-            ent, userid = i.split()
-            state.append([ent, userid])
-        elif i.startswith('Change'):
-            ent, userid, nick = i.split()
-            dic[userid] = nick
-        
-    answer = []
-    for i in state:
-        ent, userid = i
-        answer.append(message(ent, userid))
+        lst.append(i.split())
     
+    for i in lst:
+        state = i[0]
+        user = i[1]
+        if state != 'Leave': # leave가 아닐 때
+            nickname = i[2]
+            members[user] = nickname
+    
+    for i in lst:
+        state = i[0]
+        user = i[1]
+        if state == 'Enter':
+            answer.append("{}님이 들어왔습니다.".format(members[user]))
+        elif state == 'Leave':
+            answer.append('{}님이 나갔습니다.'.format(members[user]))
     return answer
-
-'''
-
-
-'''
