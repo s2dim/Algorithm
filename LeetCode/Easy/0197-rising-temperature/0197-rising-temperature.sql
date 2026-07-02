d5cb79a9027023a2c7716a06344784b25d86047d
s2dim <120532339+s2dim@users.noreply.github.com>
2026-07-02 15:48:18 +0900

    Time: 834 ms (5.35%), Space: 0B (100%) - LeetHub

--- LeetCode/Easy/0197-rising-temperature/0197-rising-temperature.sql
+++ LeetCode/Easy/0197-rising-temperature/0197-rising-temperature.sql
@@ -1,5 +1,14 @@
 # Write your MySQL query statement below
 
-select w1.id
-from weather w1 left join weather w2 on w1.recordDate = date_add(w2.recordDate, interval 1 day)
-where w1.temperature > w2.temperature
+-- select w1.id
+-- from weather w1 left join weather w2 on w1.recordDate = date_add(w2.recordDate, interval 1 day)
+-- where w1.temperature > w2.temperature
+
+with yesterday as(
+    select id, recorddate, temperature, lag(temperature) over(order by recorddate) as temp2, lag(recorddate) over(order by recorddate) as date2
+    from weather 
+)
+
+select id
+from yesterday
+where recorddate = date_add(date2, interval 1 day) and temperature > temp2
