4967ae5207484f7aae3fb10469d826b94f65b44f
s2dim <120532339+s2dim@users.noreply.github.com>
2026-07-02 20:18:10 +0900

    [level 3] Title: 단어 변환, Time: 2.09 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,44 +1,24 @@
 from collections import deque
-
 def solution(begin, target, words):
-    
-    def diff(a, b):
-        a = [i for i in a]
-        b = [i for i in b]
-        
-        cnt = 0
-        for i in range(len(a)):
-            if a[i] != b[i]:
-                cnt += 1
-            
-            if cnt > 1:
-                break
-        
-        if cnt == 1:
-            return True
-        else:
-            return False
-    
-    if target not in words:
-        return 0 
-    
-    queue = deque([(0, begin)])
+    answer = 0
+    queue = deque([(begin, 0)])
+    n = len(begin)
     
     while queue:
-        dist, q = queue.popleft()
+        q, idx = queue.popleft()
         
         if q == target:
-            return dist
-        
-        if dist > len(words):
+            return idx
+        if idx >= len(words):
             return 0
         
         for i in words:
-            if diff(q, i):
-                queue.append((dist+1, i))
+            cnt = 0
+            for j in range(n):
+                if q[j] == i[j]:
+                    cnt += 1
+            if cnt == n-1:
+                queue.append((i, idx+1))
+    
 
-
-
-'''
-
-'''
+    return answer
